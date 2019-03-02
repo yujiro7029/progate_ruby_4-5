fa912e0ce057aea095499ebba3849ba2cf372454
ユーザー名 <kawa0320@kawanamiyuujirounoMacBook-Pro.local>
2019-03-02 18:49:06 +0900

    drink.rbを追加しました

---
+++ drink.rb
@@ -0,0 +1,14 @@
+require "./menu"
+
+class Drink < Menu
+attr_accessor :amount
+
+ def initialize(name:, price:, amount:)
+   super(name: name, price: price,)
+   self.amount = amount
+ end
+
+ def info
+   return "#{self.name} #{self.price}円(#{self.amount}mL)"
+ end
+end
