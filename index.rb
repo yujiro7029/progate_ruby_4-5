c5a4149d0a381d538e0a68f443a6b426a3b0b93c
ユーザー名 <kawa0320@kawanamiyuujirounoMacBook-Pro.local>
2019-03-02 17:05:00 +0900

    menu.rb作成しました

---
+++ index.rb
@@ -0,0 +1,4 @@
+require "./food"
+require "./drink"
+
+puts "日曜日限定１００円割引セール実施中"
---
+++ menu.rb
@@ -0,0 +1,28 @@
+require "date"
+
+class Menu
+  attr_accessor :name
+  attr_accessor :price
+
+ def initialize (name:,price:)
+  self.name = name
+  self.price = price
+ end
+ def info
+   return "#{self.name}#{self.price}円"
+ end
+ def get_total_price(count)
+ total_price = self.price * count
+ if count >= 3
+    total_price -= 100
+ end
+ if count >= 1 && Menu.is_discount_day?
+   total_price -= 100
+ end
+ return total_price
+ end
+ def Menu.is_discount_day?
+   today= Date.today
+   return today.sunday?
+ end
+end
